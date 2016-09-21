060ae7f5bea6a2bdde96f01ce4c5db359ef76e4e
PaulRedmond94 <paulredmond1994@gmail.com>
2016-09-21 16:52:24 +0100

    New Python

--- test.py
+++ test.py
@@ -1,2 +1,52 @@
+def myFunction():
+	print("This is in my function!\n")
+
 print("Hello")
+
+print(2+2)
+
+print(4/3)
+
+name = "Paul"
+
+print("Hello " + name)
+
+print("P\na\nu\nl")
+
+print(name [0:2])
+
+array= [1,2,3,4,5,6]
+
+print (array[1])
+
+
+if 2>5:
+	print ("2 is bigger than 5")
+
+else:
+	print ("5 is bigger than 2")
+
+
+pythons = [ "cleese", "chapman", "jones" , "gilliam" ,"palin"]
+
+for p in pythons[:]: 
+	print(p)
+
+
+
+for i in range(5,10):
+	print (i)
+
+
+for i in range(1,10):
+	if i == 6:
+		print ("hit")
+		break
+	else:
+		print("miss")
+
+
+
+myFunction()
+
 
